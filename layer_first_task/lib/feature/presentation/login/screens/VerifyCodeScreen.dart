980c0d8e068f0d41192396a0579b9fae8d34b020
hdsneshati <hadisneshati1384@gmail.com>
2025-07-14 02:52:51 +0330

    start new page

---
+++ layer_first_task/lib/feature/presentation/login/screens/VerifyCodeScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyCodeScreen extends StatefulWidget {
+  const VerifyCodeScreen({super.key});
+
+  @override
+  State<VerifyCodeScreen> createState() => _VerifyCodeScreenState();
+}
+
+class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
