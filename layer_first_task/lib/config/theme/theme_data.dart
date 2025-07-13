fd8f2ab962684d5b6115d3469dc112f5d0a0eb94
hdsneshati <hadisneshati1384@gmail.com>
2025-07-14 00:59:54 +0330

    add theme in config

---
+++ layer_first_task/lib/config/theme/theme_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:layer_first_task/config/theme/color_pallet.dart';
+import 'package:layer_first_task/config/theme/text_styles.dart';
+
+ThemeData lightThemeData = ThemeData(
+    fontFamily: 'dana',
+    colorScheme: ColorPallet.lightColorScheme,
+    textTheme: lightTextTheme,
+    useMaterial3: true,
+    );
