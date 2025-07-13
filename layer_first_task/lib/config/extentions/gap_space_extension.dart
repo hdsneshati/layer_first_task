482c3a3ca854e8bf38aae894116def4dd5ddd595
hdsneshati <hadisneshati1384@gmail.com>
2025-07-14 01:43:28 +0330

    add space extentions

---
+++ layer_first_task/lib/config/extentions/gap_space_extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension EmptySpaceExtention on double {
+  SizedBox get verticalSpace => SizedBox(
+        height: toDouble(),
+      );
+  SizedBox get horizontalSpace => SizedBox(
+        width: toDouble(),
+      );
+}
+
