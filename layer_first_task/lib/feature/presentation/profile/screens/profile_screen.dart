4c759c74fd17a49d16a9f3d4ede086e8fba52b24
hdsneshati <hadisneshati1384@gmail.com>
2025-07-15 01:34:29 +0330

    add title

--- layer_first_task/lib/feature/presentation/profile/screens/profile_screen.dart
+++ layer_first_task/lib/feature/presentation/profile/screens/profile_screen.dart
@@ -1,11 +1,42 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:layer_first_task/config/asset/icons_path.dart';
+import 'package:layer_first_task/config/asset/img_path.dart';
+import 'package:layer_first_task/config/extentions/gap_space_extension.dart';
+import 'package:layer_first_task/config/theme/color_pallet.dart';
+import 'package:layer_first_task/config/theme/text_styles.dart';
 
 class ProfileScreen extends  StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          81.0.verticalSpace,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              250.0.horizontalSpace,
+              Text('مدیریت سیستم',
+              style: lightTextTheme.titleMedium,),
+            ],
+          ),
+          Row(children: [
+            SvgPicture.asset(
+              IconPath.arrow,
+              width: 40,
+              height: 40,
+              color: ColorPallet.lightColorScheme.onSurface,
+              ),
+            SvgPicture.asset(
+              ImgPath.calendar,
+            ),
+         ] )
+        ],
+      ),
+    );
   }
 }
