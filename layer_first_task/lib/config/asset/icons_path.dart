535f64bc3cad09f489e1ec3c8e6484def4acd4ef
hdsneshati <hadisneshati1384@gmail.com>
2025-07-17 14:37:08 +0330

    assets new icons

--- layer_first_task/lib/config/asset/icons_path.dart
+++ layer_first_task/lib/config/asset/icons_path.dart
@@ -50,4 +50,14 @@ class IconPath {
   static const String memberFilled = "assets/icons/member_filled.svg";
 
   static const String profileMarketing = "assets/icons/marketing.svg";
+ 
+ 
+  static const String hanger = "assets/icons/hanger.svg";
+  static const String profile2user = "assets/icons/profile-2user.svg";
+  static const String securitysafe = "assets/icons/security-safe.svg";
+  static const String videoplay = "assets/icons/video-play.svg";
+  
+
+
+  
 }
