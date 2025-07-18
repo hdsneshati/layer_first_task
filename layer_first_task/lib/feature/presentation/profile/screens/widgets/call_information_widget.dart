87e4e49393b41ef957dac16900fbd1cfd8c3e8b4
hdsneshati <hadisneshati1384@gmail.com>
2025-07-18 16:40:52 +0330

    finish

---
+++ layer_first_task/lib/feature/presentation/profile/screens/widgets/call_information_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:layer_first_task/config/asset/icons_path.dart';
+import 'package:layer_first_task/config/asset/svg_path.dart';
+import 'package:layer_first_task/config/extentions/gap_space_extension.dart';
+import 'package:layer_first_task/feature/presentation/profile/screens/profile_screen.dart';
+
+class CallInformationWidget extends StatelessWidget {
+  const CallInformationWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        24.0.horizontalSpace,
+        InkWell(
+          child: Container(
+            width: 148,
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color:Theme.of(context).colorScheme.secondary, // 🎨 رنگ خط دور
+                width: 1, // ضخامت خط
+              ),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  width: 20,
+                  height: 20,
+                  IconPath.wrraper,
+                ),
+                Text(
+                  'خروجی اطلاعات',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Spacer(),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            child: SvgPicture.asset(
+              IconPath.call,
+              width: 20,
+              height: 20,
+              color: Theme.of(context).colorScheme.onSecondary,
+            ),
+          ),
+        ),
+        10.0.horizontalSpace,
+      ],
+    );
+  }
+}
